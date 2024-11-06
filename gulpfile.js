72635adc6738f7083515bd01bc12a9f2381d9ba8
Wkatir <110741874+wkatir@users.noreply.github.com.>
2024-11-06 13:34:28 -0600

    feat(video): styling and structure the video section

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
-import {src, dest, watch} from 'gulp';
-import gulpSass from "gulp-sass";
-import * as dartSass from 'sass';
+import { src, dest, watch } from 'gulp';
+import gulpSass from 'gulp-sass';
+import * as sass from 'sass';
 
-const sass = gulpSass(dartSass);
+const compileSass = gulpSass(sass);
 
-export function css(done){
+export function css(done) {
     src('src/scss/app.scss')
-        .pipe(sass().on("error", sass.logError))
-        .pipe(dest('build/css'))
-        
+        .pipe(compileSass().on('error', compileSass.logError))
+        .pipe(dest('build/css'));
+
     done();
 }
 
-export function dev(){
-    watch('src/scss/**/*.scss', css)
+export function dev() {
+    watch('src/scss/**/*.scss', css);
 }
