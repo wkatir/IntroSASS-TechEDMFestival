ba5ca31ccb224baf44b5fc477b196a5b8207b77b
Wkatir <110741874+wkatir@users.noreply.github.com.>
2024-11-09 08:13:11 -0600

    feat: create README.md short description and fixing some features

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 import { src, dest, watch, series } from 'gulp';
 import gulpSass from 'gulp-sass';
 import * as sass from 'sass';
+import cleanCSS from 'gulp-clean-css';
+import terser from 'gulp-terser';
 
 const compileSass = gulpSass(sass);
 
 export function js(done) {
     src('src/js/**/*.js')
+        .pipe(terser()) 
         .pipe(dest('build/js'));
     done();
 }
@@ -13,6 +16,7 @@ export function js(done) {
 export function css(done) {
     src('src/scss/**/*.scss', { sourcemaps: true })
         .pipe(compileSass().on('error', compileSass.logError))
+        .pipe(cleanCSS()) 
         .pipe(dest('build/css', { sourcemaps: '.' }));
     done();
 }
